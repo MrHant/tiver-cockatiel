dd04c3f77f77c82889e3cfd8a37e84eb857f7dcf
Artsiomi Silivonchyk <mr.hant@gmail.com>
2018-12-20 23:58:03 +0300

    Add new solution files

---
+++ RequestTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace tiver_cockatiel
+{
+    [TestFixture]
+    public class RequestTests
+    {
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}
