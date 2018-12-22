5fe318a773ea3eebb61c5277762dc1035eea1ed3
Artsiomi Silivonchyk <mr.hant@gmail.com>
2018-12-23 01:36:18 +0300

    Fix issue with missing leading zero in batch file

--- execute.bat
+++ execute.bat
@@ -1,2 +1,3 @@
-.\libs\nunit.consolerunner.3.9.0\tools\nunit3-console.exe tiver_cockatiel.dll --result ./output/%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%_TestResult.xml
+set temptime=%TIME: =0%
+.\libs\nunit.consolerunner.3.9.0\tools\nunit3-console.exe tiver_cockatiel.dll --result ./output/%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%temptime:~0,2%%temptime:~3,2%%temptime:~6,2%_TestResult.xml
 pause
